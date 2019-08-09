ed52baf11fb82adc8381ffc989412e993ed5fe14
jigracia <jigracia@miuandes.cl>
2019-08-09 01:20:36 -0400

    first commit

---
+++ ProyectoPrueba01/ProyectoPrueba01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPrueba01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Goodbye World");
+            Console.WriteLine("Yeahhhhhhhhh Boiiiiiiiiiiii");
+        }
+    }
+}
